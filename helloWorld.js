37eaa630809f0e86a01773c817329cb4fa402220
Michael Langford <mikelangford@licsystems.com>
2022-06-17 12:47:42 -0700

    opening files

---
+++ helloWorld.js
@@ -0,0 +1,15 @@
+import React from `react`;
+import {Text, View} from `react-native`;
+
+const yourApp = () => {
+    return (
+        <View style={{ flex:1, justifyContent: `center`, alignItems:'center'}}>
+                <Text> 
+        Try editing me
+        </Text>
+        </View>
+
+    );
+}
+
+export default yourApp;

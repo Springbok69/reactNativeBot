c9bd4c13b46103bbcd43accfa675d790e2f13a88
Michael Langford <mikelangford@licsystems.com>
2022-06-20 09:59:16 -0700

    added a new green button with styling

--- BotSwarm/App.js
+++ BotSwarm/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
@@ -7,9 +7,22 @@ export default function App() {
       <Text>botSwarm</Text>
       <Text>bot connected</Text>
       <StatusBar style="auto" />
+      <View>
+      <TouchableOpacity
+           style={styles.loginScreenButton}
+           onPress={() => console.log("I was pressed")}
+           underlayColor='#fff'>
+           <Text style={styles.loginText}>Login</Text>
+      </TouchableOpacity>
+
+        
+      </View>
     </View>
   );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +31,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  loginScreenButton: {
+    marginRight:40,
+    marginLeft:40,
+    marginTop:10,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'#1E6738',
+    borderRadius:10,
+    borderWidth: 2,
+    borderColor: '#fff'
+  },
+  loginText:{
+    color:'#fff',
+    textAlign:'center',
+    paddingLeft : 10,
+    paddingRight : 10
+}
 });

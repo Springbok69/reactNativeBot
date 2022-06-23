f160a6c86d24d5d9416bf0f8c50418c642fa5fc4
Michael Langford <mikelangford@licsystems.com>
2022-06-23 08:32:35 -0700

    added new app JS

--- BotSwarm/App.js
+++ BotSwarm/App.js
@@ -1,19 +1,26 @@
+
 import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
+
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Home Screen</Text>
+    <Button
+      title="Go to Details"
+      onPress={() => navigation.navigate('Details')}
       />
     </View>
   );
 }
+
+
+
+
 
 function DetailsScreen() {
   return (
@@ -23,17 +30,29 @@ function DetailsScreen() {
   );
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+
       </Stack.Navigator>
     </NavigationContainer>
-  );
-}
+  )
+};
 
 export default App;
